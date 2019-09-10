0d87548f1a4cb4a64797f923ede4d131f607ba3e
bargross <barggros@gmail.com>
2019-09-10 22:29:08 +0100

    cleaned Either

--- Java/ArrayList/src/com/company/ArrayList.java
+++ Java/ArrayList/src/com/company/ArrayList.java
@@ -47,43 +47,6 @@ public class ArrayList<T extends Object> implements List<T> {
         container[size++] = value;
     }
 
-    public void bucketSort(T[] container) {
-        java.util.List<T>[] tempContainer = (java.util.ArrayList<T>[])new java.util.ArrayList[container.length];
-        for(int index = 0; index < container.length; ++index) {
-            int bucketIndex = container.length * container[index].hashCode();
-            if(tempContainer[bucketIndex] == null) {
-                tempContainer[bucketIndex] = new java.util.ArrayList<T>();
-                tempContainer[bucketIndex].add((T)container[index]);
-            }
-
-        }
-
-        for(int index = 0; index < container.length; ++index) {
-            if(tempContainer[index] != null) {
-                java.util.List<T> temp = (java.util.ArrayList<T>)tempContainer[index];
-                Collections.sort(temp, new Comparator<T>() {
-                    @Override
-                    public int compare(T t, T t1) {
-                        if(t.hashCode() > t1.hashCode())
-                            return 1;
-                        else if(t.hashCode() < t1.hashCode())
-                            return 0;
-                        else return -1;
-                    }
-                });
-            }
-        }
-
-        int iOrderedElement = 0;
-        for(int index=0; index < container.length; ++index) {
-            List<T> temp = ((List<T>)container[index]);
-            if(temp == null) continue;
-            for(int iContainer = 0; iContainer < temp.size(); ++iContainer) {
-                container[iOrderedElement++] = temp.get(iContainer);
-            }
-        }
-    }
-
     public void bucketSort(Collection<T> collection) {
         T[] collectionArray = (T[])collection.toArray();
         bucketSort(collectionArray);
@@ -145,5 +108,42 @@ public class ArrayList<T extends Object> implements List<T> {
             temp[index] = get(index);
         }
         container = temp;
+    }
+
+    private void bucketSort(T[] container) {
+        java.util.List<T>[] tempContainer = (java.util.ArrayList<T>[])new java.util.ArrayList[container.length];
+        for(int index = 0; index < container.length; ++index) {
+            int bucketIndex = container.length * container[index].hashCode();
+            if(tempContainer[bucketIndex] == null) {
+                tempContainer[bucketIndex] = new java.util.ArrayList<T>();
+                tempContainer[bucketIndex].add((T)container[index]);
+            }
+
+        }
+
+        for(int index = 0; index < container.length; ++index) {
+            if(tempContainer[index] != null) {
+                java.util.List<T> temp = (java.util.ArrayList<T>)tempContainer[index];
+                Collections.sort(temp, new Comparator<T>() {
+                    @Override
+                    public int compare(T t, T t1) {
+                        if(t.hashCode() > t1.hashCode())
+                            return 1;
+                        else if(t.hashCode() < t1.hashCode())
+                            return 0;
+                        else return -1;
+                    }
+                });
+            }
+        }
+
+        int iOrderedElement = 0;
+        for(int index=0; index < container.length; ++index) {
+            List<T> temp = ((List<T>)container[index]);
+            if(temp == null) continue;
+            for(int iContainer = 0; iContainer < temp.size(); ++iContainer) {
+                container[iOrderedElement++] = temp.get(iContainer);
+            }
+        }
     }
 }
